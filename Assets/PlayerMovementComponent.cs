1cd753fc4c50f57240dc5e78cf0d7b04bfd9ec80
Rafal Ozog <rafalozog@gmail.com>
2018-06-20 11:06:18 +0100

    Project Upload

---
+++ Assets/PlayerMovementComponent.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementComponent : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+    private float maxVelocity;
+    private Vector2 velocity;
+
+    private SpriteRenderer spriteR;
+    private Sprite[] sprites;
+
+    // -1 means 'left'; '1' means 'right'
+    private int direction;
+
+    private Vector2 buttonLeftPosition;
+
+    int acc;
+    int spriteChangeFreezer;
+    int spriteFreezingValue;
+
+    // Use this for initialization
+    void Start() {
+
+        rb2d = GetComponent<Rigidbody2D>();
+        maxVelocity = 10.0f;
+        velocity = new Vector2(0.0f, 0.0f);
+
+        spriteR = GetComponent<SpriteRenderer>();
+        sprites = Resources.LoadAll<Sprite>("simonSpritesheetAr15");
+        direction = 1;
+
+        acc = 0;
+        spriteFreezingValue = 20;
+        spriteChangeFreezer = spriteFreezingValue;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void moveLeft()
+    {
+        velocity = new Vector2(-4.5f, 0.0f);
+        if (rb2d.velocity.x > -3.5f)
+        {
+            rb2d.AddForce(velocity, ForceMode2D.Impulse);
+        }
+
+        // Spritesheet
+        direction = -1;
+
+        if (spriteChangeFreezer == 0)
+        {
+            if (acc % 3 == 0)
+                spriteR.sprite = sprites[5];
+            else if (acc % 3 == 1)
+                spriteR.sprite = sprites[6];
+            else
+                spriteR.sprite = sprites[7];
+
+            spriteChangeFreezer = spriteFreezingValue;
+            acc++;
+        }
+    
+        spriteChangeFreezer--;
+    }
+
+    public void moveRight()
+    {
+        velocity = new Vector2(4.5f, 0.0f);
+        if (rb2d.velocity.x < 3.5f)
+        {
+            rb2d.AddForce(velocity, ForceMode2D.Impulse);
+        }
+
+        // Spritesheet
+        direction = 1;
+
+        if (spriteChangeFreezer == 0)
+        {
+            if (acc % 3 == 0)
+                spriteR.sprite = sprites[1];
+            else if (acc % 3 == 1)
+                spriteR.sprite = sprites[2];
+            else
+                spriteR.sprite = sprites[3];
+
+            spriteChangeFreezer = spriteFreezingValue;
+            acc++;
+        }
+
+        spriteChangeFreezer--;
+    }
+
+    public void stopMoving()
+    {
+        rb2d.velocity = new Vector2(0.0f, rb2d.velocity.y);
+
+        // Spritesheet
+        if (direction == 1)
+            spriteR.sprite = sprites[0];
+        else
+            spriteR.sprite = sprites[4];
+    }
+
+    public void jump()
+    {
+        velocity = new Vector2(0.0f, 24.0f);
+        rb2d.AddForce(velocity, ForceMode2D.Impulse);
+    }
+}
---
+++ Assets/UiControlComponent.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiControlComponent : MonoBehaviour
+{
+    private PlayerMovementComponent playerMovementComponent;
+
+    private Vector2 movementButtonLeftPosition;
+    private Vector2 movementButtonRightPosition;
+
+    void Start ()
+    {
+        playerMovementComponent = GetComponent<PlayerMovementComponent>();
+
+        movementButtonLeftPosition = GameObject.Find("movementButtonLeft").transform.position;
+        movementButtonRightPosition = GameObject.Find("movementButtonRight").transform.position;
+    }
+	
+	
+	void FixedUpdate ()
+    {
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (!Input.GetKey(KeyCode.D))
+                playerMovementComponent.moveLeft();
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            if (!Input.GetKey(KeyCode.A))
+                playerMovementComponent.moveRight();
+        }
+        else
+            playerMovementComponent.stopMoving();
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            playerMovementComponent.jump();
+        
+    
+        if (Input.touchCount > 0)
+        {
+            Vector2 touchPosition = Input.GetTouch(0).position;
+
+            if (touchPosition.x > movementButtonLeftPosition.x - 200 && touchPosition.x < movementButtonLeftPosition.x + 200)
+                if (touchPosition.y > movementButtonLeftPosition.y - 200 && touchPosition.y < movementButtonLeftPosition.y + 200)
+                    playerMovementComponent.moveLeft();
+
+            if (touchPosition.x > movementButtonRightPosition.x - 200 + 1000 && touchPosition.x < movementButtonRightPosition.x + 200 + 1000)
+                if (touchPosition.y > movementButtonRightPosition.y - 200 && touchPosition.y < movementButtonRightPosition.y + 200)
+                    playerMovementComponent.moveRight();
+        }
+    }
+}
